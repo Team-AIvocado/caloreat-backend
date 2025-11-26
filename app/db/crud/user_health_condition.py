a8906573ab5687374d8413870c595dca1d930c0f
hyunjun <nujnuyh@gmail.com>
2025-11-26 21:12:26 +0900

    Revert "add tmp mockup api user_profile/ semi-rollback"
    This reverts commit 7180cbd0312bec5146e01f8f5675f9053c73a082.

--- app/db/crud/user_health_condition.py
+++ app/db/crud/user_health_condition.py
@@ -1,9 +1,4 @@
-from fastapi import HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from app.db.models.user_profile import UserProfile
-from app.db.schemas.user_profile import UserProfileCreate
-from typing import Optional, List
+from app.db.database import Base
 
 
 # 건강 및 식이 제한정보 user_health_conditions
